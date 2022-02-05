f7c38d09be7d0a84c2dadc79fe4bc9150a5ea205
Jamie Anderson <jamie@serenade.co>
2022-02-05 17:49:55 +1100

    Refactored Footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import { SocialLinks } from '../common';
+
+export function Footer() {
+  return (
+    <div className='footer flex justify-center items-center py-4'>
+      <SocialLinks footer />
+    </div>
+  );
+}
